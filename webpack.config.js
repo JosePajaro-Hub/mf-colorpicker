552559ae0d053c7def2e0b97680a9681f0e160d1
JosePajaro-Hub <jose.pajaro@gse.com.co>
2024-04-03 23:21:47 -0500

    pathChange

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3001/",
+    publicPath: "https://main--steady-cannoli-e3a6e7.netlify.app/",
   },
 
   resolve: {
